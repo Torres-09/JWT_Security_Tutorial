4a117d88bbe5a68854d70d9d177a17314d4b197f
Torres-09 <dla612@naver.com>
2022-09-26 23:58:12 +0900

    feat : UserEntity , AuthorityEntity 추가 feat : SpringSecurity 관련 설정 추가

--- src/main/java/me/hwan2da/jwttutorial/config/SecurityConfig.java
+++ src/main/java/me/hwan2da/jwttutorial/config/SecurityConfig.java
@@ -1,9 +1,12 @@
 package me.hwan2da.jwttutorial.config;
 
+import org.jetbrains.annotations.NotNull;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.web.SecurityFilterChain;
 
 import static org.springframework.security.config.Customizer.withDefaults;
@@ -12,6 +15,13 @@ import static org.springframework.security.config.Customizer.withDefaults;
 @Configuration
 public class SecurityConfig {
 
+    @Bean
+    public WebSecurityCustomizer webSecurityCustomizer(){
+        return (web) -> web.ignoring().antMatchers(
+                "/h2-console/**"
+                , "/favicon.ico"
+        );
+    }
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
