36314606d25bd0197b02a7fe8fe5933063f9209e
imsehwan <dla612@naver.com>
2022-09-29 01:56:34 +0900

    feat : dto 추가 fix : table 명 접미사 _tb 추가 fix : entity 필드명 카멜식 표기법으로 변경

---
+++ src/main/java/me/hwan2da/jwttutorial/dto/LoginDto.java
@@ -0,0 +1,20 @@
+package me.hwan2da.jwttutorial.dto;
+
+import lombok.*;
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class LoginDto {
+    @NotNull
+    @Size(min = 3, max = 50)
+    private String userName;
+    @NotNull
+    @Size(min = 3, max = 100)
+    private String password;
+}
---
+++ src/main/java/me/hwan2da/jwttutorial/dto/SignUpDto.java
@@ -0,0 +1,27 @@
+package me.hwan2da.jwttutorial.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.*;
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class SignUpDto {
+    @NotNull
+    @Size(min = 3, max = 50)
+    private String userName;
+
+    @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
+    @NotNull
+    @Size(min = 3, max = 100)
+    private String password;
+
+    @NotNull
+    @Size(min = 3,max = 50)
+    private String nickname;
+}
