9ac377a60379bcf44a524aaa812e23e0e58f2759
imsehwan <dla612@naver.com>
2022-09-28 22:46:28 +0900

    feat : 401 authentication error feat : jwtSecurityConfig(jwt custom filter 추가)

--- src/main/java/me/hwan2da/jwttutorial/jwt/jwtSecurityConfig.java
+++ src/main/java/me/hwan2da/jwttutorial/jwt/jwtSecurityConfig.java
@@ -1,15 +1,25 @@
 package me.hwan2da.jwttutorial.jwt;
 
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.SecurityConfigurer;
+import org.springframework.security.config.annotation.SecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.DefaultSecurityFilterChain;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
-public class jwtSecurityConfig{
+@Configuration
+public class jwtSecurityConfig extends SecurityConfigurerAdapter<DefaultSecurityFilterChain,HttpSecurity> {
     private TokenProvider tokenProvider;
 
     public jwtSecurityConfig(TokenProvider tokenProvider) {
         this.tokenProvider = tokenProvider;
     }
 
+    @Override
+    public void configure(HttpSecurity http) throws Exception {
+        JwtFilter customFilter = new JwtFilter(tokenProvider);
+        http.addFilterBefore(customFilter, UsernamePasswordAuthenticationFilter.class);
+    }
 }
